a87ab92c19cc2c87c0ba65303b5772b6f50eadd4
Douglas Gabriel da Costa Alves <104402297+Kirikazu22@users.noreply.github.com>
2023-02-03 12:25:59 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <windows.h>
+ 
+using namespace std;
+ 
+struct Registro
+{
+ string nome;
+ int codigo;
+ string tarja;
+ double a;
+ double b;
+};
+
+void inserir(){
+ //inserir um dado
+}
+
+void alterar(){
+ //alterar um dado
+}
+
+void deletar(){
+ //deletar um dado
+}
+
+void imprimir(){
+ //impimir os campos
+}
+
+void ordenar(){
+ //ordenar por um ou mais campos
+}
+
+int gravar(){
+    //gravar dados em binário
+}
+
+int main() {
+    
+//chamada de funções
+}
