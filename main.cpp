2fda13a46a40f586886c75bac3512bab9b3f998f
Douglas Gabriel da Costa Alves <104402297+Kirikazu22@users.noreply.github.com>
2023-02-12 18:45:52 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ struct Data {
 
 // Gera o arquivo binário
 void generateBin() {
-    ifstream base("base.csv", ios_base::in);
-    ofstream baseBin("base.bin", ios::binary);
+    ifstream base("base16_OK.csv", ios_base::in);
+    ofstream baseBin("base16_OK.bin", ios::binary);
     string aux;
 
     if(base.is_open()){
@@ -61,38 +61,71 @@ void generateBin() {
     } else cout << "Nao foi possivel abrir o arquivo base.csv" << endl;
 }
 
-void insert(){
- //inserir um dado
+void selectionSort(Data vetor[], int tam){
+    int menor; 
+    Data a;
+
+    for (int indice = 0; indice < tam-1; indice++) {
+   	 menor = indice;
+   	 for (int j = indice + 1; j < tam; j++) {
+   		 if (vetor[j].fourth < vetor[menor].fourth){
+   			 menor = j;
+   		 }
+   	 }
+   	 a = vetor[indice];
+   	 vetor[indice] = vetor[menor];
+   	 vetor[menor] = a;
+    }
 }
 
-void delet(){
- //deletar um dado
+void insert(Data *data, int size){
+    ofstream baseBin("base16_OK.bin", ios::binary);
+    
+
+        for(int i = 0; i < size; i++) {
+            // É uma biblioteca que permite trabalhar com strings de forma mais fácil, permitindo que você leia a string como uma linha de arquivo 
+           
+
+            // Armazena a informação da struct Data no arquivo binário
+            baseBin.write(reinterpret_cast<char*>(&data[i]), sizeof(Data));
+        }
+
+        baseBin.close();
 }
 
-void show(){
- //impimir os campos
-}
-
-void sort(){
-
-}
-/*
-void alterar(){
+void deletar(){
 
 }
 
-void read(){
+void showTable(Data readData, int size){
+    int aux = 0;
+    ifstream baseBin("base16_OK.bin", ios_base::binary);
+    Data *data = new Data[size];
 
+    while (baseBin.read(reinterpret_cast<char*>(&readData), sizeof(Data))) {
+        data[aux] = readData;   
+        aux++;
+    }
+    
+    for(int i = 0; i < size; i++){
+        cout << data[i].first << endl;
+        cout << data[i].second << endl;
+        cout << data[i].third << endl;
+        cout << data[i].fourth << endl;
+        cout << data[i].fifth << endl;
+    }
+
+    baseBin.close();
 }
 
-void write(){
-
+void showMenu(){
+    cout << "\n\t\t\tSISTEMA DE CADASTROS\n\n1-Inserir\t\t2-Deletar\t\t3-Imprimir\n4-Ordenar por preco\t5-Ordenar por nome\t0-Sair\n";
 }
-*/
+
 int main(void) {
 	generateBin();
 
-    ifstream baseBin("base.bin", ios_base::binary);
+    ifstream baseBin("base16_OK.bin", ios_base::binary);
     Data readData;
 
     if(baseBin.is_open()){
@@ -103,7 +136,7 @@ int main(void) {
         // To fechando o arquivo pois com o metodo seekg não funciona
         // Juliana disse que vai me ajudar a encontrar o error
         baseBin.close();
-        baseBin.open("base.bin", ios_base::binary);
+        baseBin.open("base16_OK.bin", ios_base::binary);
 
         if(baseBin.is_open()){
             Data *data = new Data[size];
@@ -117,6 +150,47 @@ int main(void) {
             
             cout << "Array de dados gerado com sucesso, pode comecar os filtros" << endl;
             // A PARTIR DAQUI É POSSÍVEL APLICAR OS FILTROS NO ARRAY DATA!
+            
+
+        //INTERFACE
+            int selector;
+            
+            do{         
+                showMenu();
+                       
+                cin >> selector;
+
+                switch (selector){
+                    //inserir
+                    case 1:
+                
+                
+                    //deletar
+                    case 2:
+
+                
+                    //imprimir
+                    case 3:
+                        showTable(readData, size);
+                
+                    //ordenar 1
+                    case 4:
+                        selectionSort(data, size);
+                        insert(data, size);
+                
+                    //ordenar 2
+                    case 5:
+
+                
+                    //erros
+                    default:
+                    
+
+                    };
+
+                //limpar da tela e voltar para função inicial
+            }while(selector != 0);
+
 
 
         } else {
